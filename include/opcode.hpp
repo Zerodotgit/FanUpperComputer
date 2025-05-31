21db9717908ab96aeebbc4afca11adcc3762ac3a
zerodots <william@zerodots.net>
2025-05-31 15:25:05 +0800

    添加完成响应识别码，开关识别码和正反转识别码

--- include/opcode.hpp
+++ include/opcode.hpp
@@ -10,14 +10,18 @@
 #define PORT 2345
 
 // ====================socket通信前缀识别码===================
-#define COMPLETE "0xFF:"    // 服务器完成客户端指令返回完成响应识别码
+#define COMPLETE "0XFF:"    // 服务器完成客户端指令返回完成响应识别码
 
 // socket连接
 #define HELLO_TO_SERVER "0X00:"    // 对应客户端连接请求的识别码
 #define HELLO_TO_CLIENT "0X01:"    // 对应服务器连接响应的识别码
 
 // 开关
-#define SWITCH_OFF "0x10:"  // 关闭风扇的识别码
-#define SWITCH_ON "0x11:";  // 开启风扇的识别码
+#define SWITCH_OFF "0X10:"  // 关闭风扇的识别码
+#define SWITCH_ON "0X11:"  // 开启风扇的识别码
+
+// 转动方向
+#define FORWARD "0X20:"  // 正转的识别码
+#define REVERSE "0X21:" // 反转的识别码
 
 #endif //OPCODE_HPP
